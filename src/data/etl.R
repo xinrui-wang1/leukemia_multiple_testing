c7c7f2d6c2331d85b9d640eff0d75c90eee7add7
xiw483 <xiw483@ucsd.edu>
2021-02-07 22:22:15 -0800

    Update etl.R

--- src/data/etl.R
+++ src/data/etl.R
@@ -1,4 +1,8 @@
 #etl.R contains functions used to extract data from different datasets
+source("src/cleaning/GolubCleaning.R")
+clean_data <- function(data, datadir, outpath=NA) {
+  data <- read.table(datadir)
+}
 
 get_data <- function(data, datadir, outpath=NA) {
   # access the selected data set
@@ -11,3 +15,4 @@ get_data <- function(data, datadir, outpath=NA) {
     write(data, outpath, sep = "\t")
   }
 }
+
