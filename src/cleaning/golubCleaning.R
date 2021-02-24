37dd7e8f243b27863764f8bf935d7d5735c09bf0
xiw483 <xiw483@ucsd.edu>
2021-02-23 17:37:52 -0800

    commit

--- src/cleaning/golubCleaning.R
+++ src/cleaning/golubCleaning.R
@@ -92,6 +92,6 @@ clean_data <- function () {
   #For dataset 2, the first 27 columns(col3 to 29) are ALL while the last 11 (col30 to 40) are AML
   
   #save the datasets
-  write.table(df_1, data_cfg$datadir1,row.names = TRUE, col.names = TRUE)
-  write.table(df_2, data_cfg$datadir2,row.names = TRUE, col.names = TRUE)
+  write.csv(df_1, data_cfg$datadir1,row.names = TRUE, col.names = TRUE)
+  write.csv(df_2, data_cfg$datadir2,row.names = TRUE, col.names = TRUE)
 }
